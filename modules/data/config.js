226234b9ccd54ce3f3aee02230992f82a6ea1744
TranVanPhat2302 <Phat2302aa@gmail.com>
2020-07-27 22:49:46 +0700

    m

--- modules/data/config.js
+++ modules/data/config.js
@@ -1,21 +1,23 @@
 const mysql = require('mysql');
 
 
-// const config = {
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'root',
-//     password: 'phat2302',
-//     database: 'tododb',
-  
-// };
+const config = {
+    // host: 'localhost',
+    // port: 3306,
+    // user: 'root',
+    // password: 'phat2302',
+    // database: 'tododb',
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'bb19d796945073',
+    password: '29b7e1bd',
+    database: 'heroku_3fcdee9ce19be88',
+};
 
 const configheroku = {
     host: 'us-cdbr-east-02.cleardb.com',
     user: 'bb19d796945073',
     password: '29b7e1bd',
     database: 'heroku_3fcdee9ce19be88',
-  
 };
 
 const qb = mysql.createConnection(config || configheroku);
